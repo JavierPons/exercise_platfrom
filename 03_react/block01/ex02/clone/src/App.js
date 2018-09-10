4771c76014334f0be94ed3317398cdeb991c3878
Javier Pons <jpescandell@hotmail.com>
2018-09-10 17:10:55 +0300

    trying to loop via object.title, price and img

--- 03_react/block01/ex02/clone/src/App.js
+++ 03_react/block01/ex02/clone/src/App.js
@@ -1,11 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+
+class App extends React.Component {
   render() {
+    const resurce = [
+      {title: 'black',
+     img:'./images/backboard.jpg',
+     price: 200},
+     {title: 'white',
+     img:'./images/chalk.jpg',
+     price: 34},
+     {title: 'red',
+     img:'./images/clound.jpg',
+     price: 100},
+     {title: 'grey',
+     img:'./images/footBlackBoard.jpg',
+     price: 70}
+  
+  ]
+  var trackTitle = resurce.map((item, index)=>{
+      return <div key='index'>
+          {item.title}
+      </div>
+
+  })
+  var trackPrice = resurce.map((price, i)=>{
+    return <div key='i'>{price.price}</div>
+
+  })
+
+  var trackImg = resurce.map((img, ind)=>{
+
+    return <img src='{img.img}'width='50'/>
+  })
     return (
-      <div className="App">
+      <div >
+        <h1>hello</h1>
+        {trackTitle}
+        {trackPrice}
+        {trackImg}
         
       </div>
     );
