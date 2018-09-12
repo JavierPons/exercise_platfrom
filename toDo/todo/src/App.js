aad4976ae152ed7312c6014182184685ec63a919
Javier Pons <jpescandell@hotmail.com>
2018-09-12 18:33:04 +0300

    add gettingdata from typing

--- toDo/todo/src/App.js
+++ toDo/todo/src/App.js
@@ -3,10 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {text:''}
+    
+  }
+  
+  
+  handleSubmit = (e) => {
+    e.preventDefault();      // prevent reload of page
+    var text = this.refs.input.value.trim()
+    this.setState({text})
+    //this.refs.input.value = '';
+    console.log(text)
+    this.todo.push(text)
+    console.log(this.todo)
+  }
+   
   render() {
+    this.todo = []
     return (
       <div className="App">
-        <input/>
+        <h2>toDo App</h2>
+        <h4>Add something to remember</h4>
+        <form onSubmit ={this.handleSubmit.bind(this)}>
+        <input ref = 'input'/> <button >+ add</button>
+        </form>
+        <div>{this.state.text}</div>
       </div>
     );
   }
