c615bf6255e34eeaa990a59cd16351bd2379fa83
Javier Pons <jpescandell@hotmail.com>
2018-09-16 14:51:25 +0300

    added img, changed some styles

--- toDo/todo/src/TodoLista.js
+++ toDo/todo/src/TodoLista.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
+import './bin.png';
 class TodoLista extends React.Component{
-    
+    deleteItem(){
+        null
+    }
     render(){
         
         let listNameFromParent =  this.props.listNameFromParent || []
        
         this.toma = listNameFromParent.map((item,i)=>{
-                return <li className='item' key={i}>{item.todo}</li>
+                return <li className='item' key={i}><img onClick={this.deleteItem().bin(this)} 
+                        className='bin' src={require('./bin.png')} alt='bin' />   {item.todo}</li>
             })
-        return(
-            <ul className='box' >{this.toma}</ul>
+                return(
+                       <ul className='box' >{this.toma}</ul>
         )
 
     }
