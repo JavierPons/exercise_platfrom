de336c05bffb94e45f536d2c330cc5913c7aa767
Javier Pons <jpescandell@hotmail.com>
2018-09-19 14:04:35 +0300

    bl01 ex2 add conditional to object, running two parametres and added via url

--- 04_express/block01/ex02/app.js
+++ 04_express/block01/ex02/app.js
@@ -2,34 +2,34 @@ const express = require('express');
 const app     = express();
 const port    = 3001;
 
+let languages = {
+    HI: 'नमस्ते दुनिया',
+    NL: 'Hallo Wereld',
+    FR: 'Bonjour le monde',
+    ES: 'Hola Mundo',
+    IT: 'Ciao Mondo',
+    CH: '你好，世界',
+    JP: 'こんにちは世界',
+    AR: 'مرحبا بالعالم',
+    EN: 'Hello world'
+}
 app.listen(port, () => {
     console.log(`server is running on port ${port} ...`);
 })
 
-app.get(`/:lang`, (req, res)=>{
-    let{ lang } = req.params;
-        
-    let languajes = {
-        HI: 'नमस्ते दुनिया',
-        NL: 'Hallo Wereld',
-        FR: 'Bonjour le monde',
-        ES: 'Hola Mundo',
-        IT: 'Ciao Mondo',
-        CH: '你好，世界',
-        JP: 'こんにちは世界',
-        AR: 'مرحبا بالعالم',
-        EN: 'Hello world'
-    }
-    for(var i in languajes){
-        if(lang !== i){
-            languajes[lang] 
-        }
-    }
+app.get('/:lang/:word', (req, res)=>{
+  let { lang } = req.params;
+  let { word } = req.params;
+        if(languages[lang]){
+             languages[lang]
+        }else{
+             languages[lang] = word;
+}
+      
+res.send(`<h1>${languages[lang]}</h1>`)
     
-    res.send(`<h1>${languajes[lang]}</h1>`)
 })
-
 
 app.get('*',(req, res)=>{
     res.send('<h1>Hello World</h1>')
-})
+ })
