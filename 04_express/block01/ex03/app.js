6f097e791506d732f6740e2054fba0cd94c45575
Javier Pons <jpescandell@hotmail.com>
2018-09-19 16:37:47 +0300

    refactoring ex03 now working

--- 04_express/block01/ex03/app.js
+++ 04_express/block01/ex03/app.js
@@ -7,30 +7,42 @@ let languages = {}
 app.listen(port, () => {
     console.log(`server is running on port ${port} ...`);
 })
-app.get('/:lang', (req,res) => {
-    let { lang } = req.params;
-    if(lang !== languages[lang])
-    res.send(`<h1>Hello World in ${lang} not found</h1>`) 
-})
-app.get('/:lang/:word', (req, res)=>{
+
+
+
+app.get('/:lang/:word', (req, res) => {
     let { lang } = req.params;
     let { word } = req.params;
-          if(languages[lang]){
-            res.send(`<h1>${languages[lang]}</h1>`)
-               
-          }else if(!languages[lang]){
-               languages[lang] = word;
-          }
-        if(languages[lang][word] === 'remove'){
-                res.send(`<h1>${lang} removed</h1>`)
-                delete languages[lang]
-          
-        }     
-        if(languages[lang]){
-                res.send(`<h1>${lang} added with the message ${word}</h1>`)
-        }
-  })
+    if(!languages[lang]){
+        languages[lang] = word;
+        res.send(`<h1>${lang} added with the message ${word}</h1>`)
+    }else{
+        res.send(`<h1>${languages[lang]}</h1>`)
+    }
 
-  
+})
+
+app.get('/delete/:lang/:word', (req,res) => {
+    let { lang } = req.params;
+    let { word } = req.params;
+    
+    delete languages[lang]
+    res.send(`<h1>${lang} removed</h1>`)
+    
+})
+
+app.get('/:lang', (req,res) => {
+    let { lang } = req.params;
+    let { word } = req.params;
+    if(lang !== languages[lang]){
+        res.send(`<h1>Hello World in ${lang} not found</h1>`)
+    }else{
+        res.send(`<h1>${languages[lang]}</h1>`)
+    }     
+})
+
+
+
+
   
   
