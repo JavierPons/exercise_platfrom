0944a2e76dab115092fafb06fc2426ff9ad6d717
Javier Pons <jpescandell@hotmail.com>
2018-09-20 19:57:09 +0300

    added some features

--- 04_express/block02/moviesDB/app.js
+++ 04_express/block02/moviesDB/app.js
@@ -4,7 +4,21 @@ const express = require('express'),
         port = 3000
 app.use(body.urlencoded({extended: true}))
 app.use(body.json());
-let db = [];
+let  db = [
+    
+    {
+    genre: "musical", 
+    movies: [
+                {title: 'movie A', year: 2000}, 
+                {title: 'movie B', year: 3000}
+            ]},
+    {
+    genre:"horror" ,
+     movies: [
+                {title: 'movie Z', year: 2020}, 
+                {title: 'movie X', year: 3030}
+            ]}
+] 
 // [{genre:musical[{name: 'what ever', rel:2008},{name: },{}]},{genre:commedy[{},{},{}]}]
 app.use('/db', (req,res) => {
     res.send('Hello World!');
@@ -12,11 +26,49 @@ app.use('/db', (req,res) => {
 
 app.post('/genre/add',(req, res) =>{
     let key = req.body.genre;
-     db.push({key:[]})  
-        console.log(key)
+    if(!key){
+    db.push({gender:key, movies:[]}) 
+     res.send(db) 
+   
+    }
+     else{
+        db.push({gender:key, movies:[]}) 
+        res.send(db)
+        
+    }
+     
+      
 })
-console.log('=====>', db)
 
+app.post('/genre/update', (req, res) => {
+       let key = req.body.genre
+       db.forEach(function(ele){
+           if(ele.genre)
+       })
+       
+       
+        
+
+})
+
+app.post('/genre/movie/add', (req,res) => {
+
+
+})
+
+app.get('/genres', (req, res) =>{
+    
+        db.forEach(function(ele) {
+            console.log(ele.genre)
+            
+        })
+
+})
+app.get('/genre/movies',(req, res) => {
+    db.forEach((ele)=>{
+        console.log(ele)
+    })
+})
 
 app.listen(port, () => {
     console.log('listening port 3000...')
