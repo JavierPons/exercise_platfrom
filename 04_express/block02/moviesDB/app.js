3111acf646181ce662131c7bca4632f85ce37472
Javier Pons <jpescandell@hotmail.com>
2018-09-20 14:18:37 +0300

    db exer not well working

--- 04_express/block02/moviesDB/app.js
+++ 04_express/block02/moviesDB/app.js
@@ -1,16 +1,37 @@
 const express = require('express'),
         app = express(),
-        bodyParser= require('body-parser'),
+        body = require('body-parser'),
         port = 3000
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-
-app.post('/db',(req, res) =>{
-    console.log('========>',req.body)
+app.use(body.urlencoded({extended: true}))
+app.use(body.json());
+let db = [];
+// [{genre:musical[{name: 'what ever', rel:2008},{name: },{}]},{genre:commedy[{},{},{}]}]
+app.use('/db', (req,res) => {
+    res.send('Hello World!');
 })
 
+app.post('/genre/add',(req, res) =>{
+    let key = req.body.genre;
+     db.push({key:[]})  
+        console.log(db)
+})
+console.log('=====>', db)
 
 
 app.listen(port, () => {
     console.log('listening port 3000...')
 })
+
+
+
+
+/* db = [
+    {musical: [
+                {title: 'movie A', year: 2000}, 
+                {title: 'movie B', year: 3000}
+            ]},
+    {horror: [
+                {title: 'movie Z', year: 2020}, 
+                {title: 'movie X', year: 3030}
+            ]}
+] */
