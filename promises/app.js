2e91967e17c70d613e6e38410a9eb9973ef86eaf
Javier Pons <jpescandell@hotmail.com>
2018-09-28 11:04:06 +0300

    getting title and year from film db api

---
+++ promises/app.js
@@ -0,0 +1,11 @@
+let objFilm;
+async function get_title_and_year (film) {
+		
+    const mov = await fetch(`https://omdbapi.com?t=${film}&apikey=thewdb`);
+    const movie = await mov.json();
+    console.log(movie.Title);
+    console.log(movie.Year);
+    return objFilm = {title:movie.Title, year:movie.Year}
+    
+
+}
