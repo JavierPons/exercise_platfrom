4eaaac1366a06a1edab65bb98f826e9ad942f109
Javier Pons <jpescandell@hotmail.com>
2018-09-30 15:01:09 +0300

    added MongoDB folder and initialized subfolders and app.js

---
+++ 04_express/block03/mongoDB/app.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    todosRoute = require('./routes/TodosRoute'),
+    bodyParser = require('body-parser');
+// =================== initial settings ===================
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+// connnect to mongo
+mongoose.connect('mongodb://127.0.0.1/newdatabase',()=>{
+    console.log('connected to mongodb');
+})
+// routes
+app.use('/todos', todosRoute);
+// Set the server to listen on port 3000
+app.listen(3000, () => console.log(`listening on port 3000`))
