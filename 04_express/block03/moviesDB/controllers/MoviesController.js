886bf67f972c8cb192285efb51eac2c2ee69f306
Javier Pons <jpescandell@hotmail.com>
2018-10-05 11:11:33 +0300

    refactoringmoviesDB

--- 04_express/block03/moviesDB/controllers/MoviesController.js
+++ 04_express/block03/moviesDB/controllers/MoviesController.js
@@ -16,15 +16,10 @@ class MovieController {
         // GET MOVIES
 
     async _movies(req,res){
+        
         try{
             const done = await Movie.find({});
             res.send(done);
-            // db.forEach(function(ele){
-            //     console. log(ele.genre, ele.movies) 
-            //      gen.push(ele.genre)
-            //      gen.push(ele.movies)
-            // })
-            // res.send(gen);
         }
         catch(e){
             res.send({e});
@@ -53,71 +48,71 @@ class MovieController {
     // UPDATE MOVIE
 
     async _update(req, res){
-        try{
-            let oldTitle = req.body.oldTitle;
-            let newTitle = req.body.newTitle;
-            let newYear = req.body.newYear;
-            let genre = req.body.genre;
-        
-        db.forEach(function(ele,i){
-                if(ele.genre == genre){
-                  db[db.indexOf(ele)].movies.forEach(function(item,ind){
-                    if(item.title == oldTitle){
-                       db[db.indexOf(ele)].movies[ind].title = newTitle;
-                       if(newYear == undefined){
-                                    console.log('there is not year given')
-                    }else{
-                        db[db.indexOf(ele)].movies[ind].year = newYear;
-                    }
-                    }
-                     
-                  })
-                      
-                }
-                
-            })
-        
-            res.send(db);   
-        }
-        catch(e){
-            res.send({e})
-        }
+        let { oldFilm, newFilm, genre, year} = req.body;
+        const genr = await Genre.findOne({genre})
+        const fil = await Movie.findOne({oldFilm})
+        if(genr){
+            if(fil){
+            try{
+                const done = await Movie.create({genre:{genre},title:{newFilm},year:{year}});
+                res.send(done);   
+            }
+            catch(e){
+                res.send({e})
+            }
+        }else{
+            console.log('Film not found. Try again!')
+        }   
+    }
 
     }
 
     async _deleteMovie(req,res){
-        try{
-            let deleteMovies = req.body.title;
-            let genre = req.body.genre;
-            db.forEach(function(ele,i){
-                if(ele.genre == genre){
-                    delete db[i].movies
-                }
-            })
+        let { genre, film, year} = req.body;
+        const genr = await Genre.findOne({genre})
+        const fil = await Movie.findOne({film})
+        if(genr){
+            if(fil){
+                    try{
+                        const done = await Movie.deleteOne({title:{fil},year:{year}});
+                        
+                        res.send(done);
+                    }
+                    catch(e){
+                        res.send({e})
+                    }
+            }else{
+                console.log('Not found that film. Try again!')
+            }
+
+            }else{
+                console.log('Genre not found. Try again')
+            }       
+        }
+
+        
     
-            res.send(db);
-        }
-        catch(e){
-            res.send({e})
-        }
-    }
 
     async _deleteGenre(req, res){
-        try{
-            let deleteGenre = req.body.deleteGenre; // one we want to delete
-            console.log(deleteGenre);
-        db.forEach(function(ele,i){
-                if(ele.genre == deleteGenre){
-                    delete db[i];
-                }
-            })
+        let { genre } = req.body;
+        const genr = await Genre.findOne({genre})
+       
+        if(genr){
+                    try{
+                        const done = await Movie.deleteOne({genre:{genr}});
+                        
+                        res.send(done);
+                    }
+                    catch(e){
+                        res.send({e})
+                    }
+            
 
-        res.send(db)
+            }else{
+                console.log('Genre not found. Try again')
+            }       
         }
-        catch(e){
-            res.send({e})
-        }
-    }
+    
 
     async _addGenre(req,res){
         let { genre}= req.body;
